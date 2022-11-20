bd974653c200ac6c5c8962e9f9098119121cd53b
楚羽幽 <cy958416459@qq.com>
2022-11-21 06:25:40 +0800

    修复管理员修改时头像上传token

--- src/controller/SystemAdminController.php
+++ src/controller/SystemAdminController.php
@@ -274,14 +274,6 @@ class SystemAdminController extends Base
                     'listType'      => 'picture-card',
                     'uploadType'    => 'image',
                     'limit'         => 1,
-                    'multiple'      => true,
-                    'action'        => 'api1/hpadmin/SystemUpload/upload',
-                    'headers'       => [
-                        'X-Requested-With'  => 'XMLHttpRequest',
-                    ],
-                    'data'          => [
-                        'dir_name'  => 'system_name',
-                    ],
                 ],
             ],
         ],
